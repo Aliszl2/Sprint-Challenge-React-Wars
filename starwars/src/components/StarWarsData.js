747ca4cbfc25c28c4e60eaddde3d4aa936ffe9ae
Alison Law <alisparklaw@gmail.com>
2020-01-24 08:40:22 +0000

    Set data to state

--- starwars/src/components/StarWarsData.js
+++ starwars/src/components/StarWarsData.js
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-
-function StarwarsData() {
-    useEffect(() => { 
-        console.log("component mounted");
-        axios.get("https://swapi.co/api/people/")
-        .then(response=>{
-            console.log(response.data.results);
-        }
-
-        ).
-        catch(error=>{console.log("error:", error)})
-
-
-
-    }, []);
-
-
-    return ("card")
-
+function StarWarsData() {
+  const [starWarsChars, setStarWarsChars] = useState([]);
+  useEffect(() => {
+    console.log("component mounted");
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => {
+        console.log(response.data.results);
+        setStarWarsChars(response.data.results);
+        console.log(starWarsChars);
+      })
+      .catch(error => {
+        console.log("error:", error);
+      });
+  }, []);
+  console.log(starWarsChars);
+  return "card";
 }
-export default StarwarsData;
+export default StarWarsData;
